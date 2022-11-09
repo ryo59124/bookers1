5ec0c61a999f5a9d59862835fce71e45e80da9b5
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-11-09 08:12:59 +0000

    error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,42 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   
   resources :books
-  
-  #get 'books/new'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+ # get 'books/:id' => 'books#show', as: 'book'
+  #get 'books' => 'books#index'
+end
+
+#get 'books/new'
   #post 'books' => 'books#create'
   #get 'books/index'
   #get 'books/show'
   #get 'books/edit'
   #get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
